1d582de59e66abe795bf60ae98c7ad5d51dd7c23
Ryan Spangler <rspangler@Ryan-Spangler.lan>
2008-09-29 01:00:23 -0700

    added logging by nick across channels

--- bot/bot.py
+++ bot/bot.py
@@ -10,8 +10,17 @@ import text
 def has(line, message):
     return line.find(message) != -1
 
-channel_name = re.compile("#[^ ]+")
-non_punctuation = re.compile("[^.,!?]+")
+class Channel:
+    def __init__(self, name):
+        self.name = name
+        self.members = []
+
+    def member_join(self, member):
+        self.members.append(member)
+
+    def member_leave(self, member):
+        if member in self.members:
+            self.members.remove(member)
 
 class Bot:
     def __init__(self, nick, logging=False):
@@ -26,9 +35,15 @@ class Bot:
         self.connected = False
         self.processing = False
         self.logging = logging
-        self.logs = {}
+        self.channel_logs = {}
+        self.member_logs = {}
 
         self.readbuffer = ''
+        self.handlers = {}
+
+        self.re_sender = re.compile('^:(\w+)(![^ ]*)?')
+        self.re_channel = re.compile("#[^ \r\n]+")
+        self.re_non_punctuation = re.compile("[^.,!?]+")
 
     def send(self, message):
         self.s.send(message + '\n')
@@ -46,49 +61,32 @@ class Bot:
         self.send('USER ' + self.identity + ' ' + self.host + ' bla :' + self.realname)
         self.send('NICKSERV IDENTIFY ' + self.identity)
 
-    def process(self):
-        self.processing = True
-        self.identify()
-
-        while self.processing:
-            line = self.s.recv(500).rstrip()
-
-            print line
-
-            if len(self.channels) == 0:
-                self.join("#dog")
-            if has(line, 'PRIVMSG'):
-                self.parse_message(line)
-
-            parts = line.split()
-            if len(parts) > 0 and parts[0] == 'PING':
-                self.send('PONG ' + parts[1])
-            
-    def open_log(self, path, channel):
+    def open_log(self, path, subject):
         try:
-            os.makedirs('logs/channels')
+            os.makedirs(path)
         except:
             print "already exists"
 
-        self.logs[channel] = open(path + channel[1:], 'a')
+        return open(path + subject, 'a')
 
     def join(self, channel):
         if self.logging:
-            self.open_log('logs/channels/', channel)
-        self.channels[channel] = True
+            self.channel_logs[channel] = self.open_log('logs/channels/', channel[1:])
+        new_channel = Channel(channel)
+        self.channels[channel] = new_channel
 
         self.send('JOIN ' + channel)
 
     def leave(self, channel):
         if self.logging:
-            self.logs[channel].close()
-            del self.logs[channel]
+            self.channel_logs[channel].close()
+            del self.channel_logs[channel]
         del self.channels[channel]
 
         self.send('PART ' + channel)
 
     def quit(self):
-        for channel in self.channels.keys:
+        for channel in self.channels.keys():
             self.leave(channel)
 
         self.processing = False
@@ -101,22 +99,68 @@ class Bot:
         self.send(response)
 
     def log(self, channel, sender, message):
-        log = '<' + sender + '> ' +  message
+        if message[:7] == 'ACTION ':
+            log = "* " + sender + " " + message[7:]
+        else:
+            log = '<' + sender + '> ' +  message
         print log
 
         if self.logging:
-            self.logs[channel].write(log + '\n')
-            self.logs[channel].flush()
+            self.channel_logs[channel].write(log + '\n')
+            self.channel_logs[channel].flush()
+
+            self.member_logs[sender].write(message + '\n')
+
+    def process(self):
+        self.processing = True
+        self.identify()
+
+        while self.processing:
+            line = self.s.recv(500).rstrip()
+
+            print line
+
+            if len(self.channels) == 0:
+                self.join("#dog")
+
+            self.parse_message(line)
 
     def parse_message(self, line):
-        complete = line[1:].split(':', 1)
-        info = complete[0].split(' ')
-        message = complete[1]
-        sender = info[0].split('!')[0]
-        channel = info[2]
+        if line[:4] == 'PING':
+            parts = line.split()
+            if len(parts) > 0 and parts[0] == 'PING':
+                self.send('PONG ' + parts[1])
 
-        self.log(channel, sender, message)
-        self.handle_message(channel, sender, message)
+        else:
+            whole = line[1:].split(':', 1)
+            info = whole[0]
+            message = ''
+            if len(whole) > 1:
+                message = whole[1]
+            sender = info.split('!')[0]
+            parts = info.split(' ')
+
+            if len(parts) > 1:
+                if parts[1] == 'PRIVMSG':
+                    channel = parts[2]
+                    self.log(channel, sender, message)
+                    self.handle_message(channel, sender, message)
+                elif parts[1] == 'JOIN':
+                    names = message.split('\r\n')
+                    channel = names[0]
+                    for name in names[1:]:
+                        self.parse_message(name)
+                    self.member_join(channel, sender)
+                elif parts[1] == 'PART':
+                    channel = parts[2]
+                    self.member_leave(channel, sender)
+                elif parts[1] == 'QUIT':
+                    self.member_quit(sender)
+                elif len(parts) > 3 and parts[3] == '=':
+                    channel = parts[4]
+                    members = message.split(' ')[:-1]
+                    
+                    self.member_names(channel, members)
 
     def handle_message(self, channel, sender, message):
         pass
@@ -145,6 +189,36 @@ class Bot:
         else:
             return [message]
 
+    def member_join(self, channel, member):
+        if self.logging:
+            self.member_logs[member] = self.open_log('logs/members/', member)
+        self.channels[channel].member_join(member)
+
+    def member_leave(self, channel, member):
+        if channel in self.channels.keys():
+            self.channels[channel].member_leave(member)
+        if self.logging and len(self.member_channels(member)) == 0:
+            self.member_logs[member].close()
+            del self.member_logs[member]
+
+    def member_quit(self, member):
+        for key in self.channels.keys():
+            self.channels[key].member_leave(member)
+        if self.logging:
+            self.member_logs[member].close()
+            del self.member_logs[member]
+
+    def member_names(self, channel, members):
+        for member in members:
+            self.member_join(channel, member)
+
+    def member_channels(self, member):
+        channels = []
+        for key in self.channels.keys():
+            if member in self.channels[key].members:
+                channels.append(key)
+
+        return channels
 
 
 class MarkovBot(Bot):
@@ -160,7 +234,7 @@ class MarkovBot(Bot):
     def handle_message(self, channel, sender, message):
         frequencies = []
         for word in message.split():
-            clean = non_punctuation.search(word).group(0)
+            clean = self.re_non_punctuation.search(word).group(0)
             if self.markov.has(clean):
                 node = self.markov.nodes[clean]
                 frequencies.append([node.data, float(node.occurrences) / self.markov.totalAtoms])
@@ -177,20 +251,26 @@ class MarkovBot(Bot):
 
             if has(message, "join"):
                 try:
-                    new_channel = channel_name.search(message).group(0)
+                    new_channel = self.re_channel.search(message).group(0)
                     self.join(new_channel)
                 except:
                     pass
             elif has(message, "leave"):
                 try:
-                    old_channel = channel_name.search(message).group(0)
+                    old_channel = self.re_channel.search(message).group(0)
                     self.leave(old_channel)
+                except:
+                    pass
+            elif has(message, "zap"):
+                try:
+                    tell_channel = self.re_channel.search(message).group(0)
+                    self.send_message(tell_channel, statement)
                 except:
                     pass
             elif has(message, "topic"):
                 topic_channel = channel
                 try:
-                    topic_channel = channel_name.search(message).group(0)
+                    topic_channel = self.re_channel.search(message).group(0)
                 except:
                     pass
                 self.send("TOPIC " + topic_channel + " :" + self.markov.generateN(11))
